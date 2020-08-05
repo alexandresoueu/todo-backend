59e46d4c38fac77729021344175dc5bb623c4226
calexandredev <calexandredev@gmail.com>
2020-08-04 21:50:18 -0300

    finish backend

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -2,10 +2,14 @@ const port = 3030
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const allowCors = require('./cors')
 
 server.use(bodyParser.urlencoded({ extended: true }))
 server.use(bodyParser.json())
+server.use(allowCors)
 
 server.listen(port, function () {
   console.log(`IT's RUNNING at port: ${port} `)
 })
+
+module.exports = server

559dd049fe0a2bf16089790dd1c320df4ed472d6
calexandredev <calexandredev@gmail.com>
2020-08-12 21:49:49 -0300

    first component dont working

---
+++ frontend/src/main/app.js
@@ -0,0 +1,14 @@
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'modules/font-awesome/css/font-awesome.min.css'
+
+import React from 'react'
+
+const App = (props) => {
+  return (
+  <div className='container'>
+    <h1>FRONTINHO</h1>
+  </div>
+  )
+}
+
+export default App

1554ab06e79f35b7c438899cfd5860cf1d85fd1d
calexandredev <calexandredev@gmail.com>
2020-08-14 20:48:57 -0300

    Form

--- frontend/src/todo/TodoForm.js
+++ frontend/src/todo/TodoForm.js
@@ -2,10 +2,18 @@ import React from 'react'
 
 const TodoForm = props => {
   return (
-    <div>
-      FORM
+    <div role='form' className='todoForm'>
+      <div className='col-xs-12 col-sm-9 col-md-10'>
+        <input id='description' className='form-control' placeholder='Add a task' />
+      </div>
+
+      <div className='col-xs-12 col-sm-3 col-md-2'>
+        <button className='btn btn-primary'>
+          <i className='fa fa-plus'></i>
+        </button>
+      </div>
     </div>
-  ) 
+  )
 }
 
 export default TodoForm

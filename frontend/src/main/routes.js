820e88e37a2066814c8cd2f30d12044c827ec20f
calexandredev <calexandredev@gmail.com>
2020-08-14 10:03:24 -0300

    working single page

--- frontend/src/main/routes.js
+++ frontend/src/main/routes.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Router, Route, Redirect, hasHistory } from 'react-router'
+import { Router, Route, Redirect, hashHistory } from 'react-router'
 
 import Todo from '../todo/todo'
 import About from '../about/about'
 
 const Routes = props => {
   return (
-  <Router history={hasHistory}>
+  <Router history={hashHistory}>
     <Route path='/all' component={Todo} />
     <Route path='/about' component={About} />
     <Redirect from='*' to='/all' />

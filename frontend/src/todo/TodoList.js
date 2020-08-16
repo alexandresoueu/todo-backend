1ac1850a6d9e2a3c0451d7cd9de6fabc32737f9d
calexandredev <calexandredev@gmail.com>
2020-08-16 16:47:50 -0300

    create delete item with button

--- frontend/src/todo/TodoList.js
+++ frontend/src/todo/TodoList.js
@@ -1,10 +1,36 @@
 import React from 'react'
+import IconButton from '../template/IconButton'
 
 const TodoList = props => {
+  const renderRows = () => {
+    const list = props.list || []
+
+    return (
+      list.map(item => (
+        <tr key={item._id}>
+          <td>{item._id}</td>
+          <td>{item.description}</td>
+          <td>
+            <IconButton style='danger' icon='trash-o' onClick={() => props.handleRemove(item)} />
+          </td>
+        </tr>
+      ))
+    )
+  }
+
   return (
-    <div>
-      <h1>LIST</h1>
-    </div>
+    <table className='table'>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Description</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderRows()}
+      </tbody>
+    </table>
   )
 }
 
--- frontend/src/todo/todo.js
+++ frontend/src/todo/todo.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
+
+const URL = 'http://localhost:3030/api/todos'
 
 class Todo extends Component {
   constructor (props) {
@@ -10,14 +14,30 @@ class Todo extends Component {
 
     this.onHandleChange = this.onHandleChange.bind(this)
     this.onHandleAdd = this.onHandleAdd.bind(this)
+    this.onHandleRemove = this.onHandleRemove.bind(this)
+
+    this.onRefresh()
   }
 
   onHandleAdd () {
-    console.log(this.state.description)
+    const description = this.state.description
+
+    axios.post(URL, { description })
+      .then(resp => this.onRefresh())
+  }
+
+  onHandleRemove (item) {
+    axios.delete(`${URL}/${item._id}`)
+      .then(resp => this.onRefresh())
   }
 
   onHandleChange (event) {
     this.setState({ ...this.state, description: event.target.value })
+  }
+
+  onRefresh () {
+    axios.get(`${URL}?sort=-createdAt`)
+      .then(resp => this.setState({ ...this.state, description: '', list: resp.data }))
   }
 
   render () {
@@ -25,7 +45,7 @@ class Todo extends Component {
       <div>
         <PageHeader name='Tasks' small='Register' />
         <TodoForm description={this.state.description} handleChange={this.onHandleChange} handleAdd={this.onHandleAdd} />
-        <TodoList />
+        <TodoList list={this.state.list} handleRemove={this.onHandleRemove} />
       </div>
     )
   }
